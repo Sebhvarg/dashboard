00cd70193db3d135e38381737fe756029ea924a5
Sebhvarg <119546442+Sebhvarg@users.noreply.github.com>
2024-11-25 09:28:11 -0500

    Agregar Indicadores

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 // Grid version 2
 import Grid from '@mui/material/Grid2'
+import IndicatorWeather from './components/IndicatorWeather'
 
 
 function App() {
@@ -11,13 +12,18 @@ function App() {
       <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 4</Grid>
-
-
-        
+        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 1
+        <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 2
+        <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 3
+        <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+        </Grid>
+        <Grid size={{ xs: 12, xl: 3 }} >Elemento: Indicador 4
+        <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+          </Grid> 
 
         {/* Tabla */}
         <Grid size={{xs: 12, xl: 8}}>Elemento: Tabla</Grid>
---
+++ src/components/IndicatorWeather.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+interface Indicator {
+    title?: String;
+    subtitle?: String;
+    value?: String;
+}
+export default function IndicatorWeather(config: Indicator) {
+    return (
+        <>
+            {config.title}<br />
+            {config.value}<br />
+            {config.subtitle}
+        </>
+    );
+}
